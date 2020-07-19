865f3ebf4510ef13d85867a76980182d1519c071
ruent <ed784@nyu.edu>
2020-07-18 22:14:27 -0400

    comm

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 #include "randomness.h"
+#include "matrix.h"
+#include <chrono>
+
 using namespace std;
 int main()
 {
-    cout<<"hey whats up?"; 
-    randomize();
-    for (int i = 0; i<10; ++i)
-        cout<< pick(1,6);
+    cout<<"hey whats up?"<<"\n"; 
+    //randomize();
+    size_t n = 1000;
+    size_t m = 1000;
+    size_t k = 1000;
+    matrix<double> A(n,m);
+    
+    for (size_t i = 0; i<n; ++i)
+    {
+        
+        for (size_t j = 0; j<m; ++j)
+            {
+                //cout<< A[i][j]<<"\n";
+                double x= pick(0.0,1.0);
+                A[i][j] = x;
+            }
+    }
+
+    matrix<double> B(m,k);
+    
+    for (size_t i = 0; i<n; ++i)
+    {       
+        for (size_t j = 0; j<m; ++j)
+            {
+                double x= pick(0.0,1.0);
+                B[i][j] = x;
+            }
+    }
+    matrix<double> C(n,k);
+    std::clock_t startcputime = std::clock();
+    productOfMatricesFast(A,B,C);
+    //cout<< trace(C)<<"\n"; 
+
+    double cpu_duration = (std::clock() - startcputime) / (double)CLOCKS_PER_SEC;
+    std::cout << "Finished in " << cpu_duration << " seconds [CPU Clock] " << std::endl;
+
 }
